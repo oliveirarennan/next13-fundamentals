ac1438c8d26ebc6c02469afb89d3ded6b9f0a1f3
Rennan Neves de Oliveira <rennan.n.oliveira@gmail.com>
2023-11-20 18:08:54 -0300

    project setup

---
+++ src/app/catalog/product/[...data]/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+interface ProductProps {
+  params: {
+    data: string[]
+  }
+}
+
+export default function Product({ params }: ProductProps) {
+  const [productId, size, color] = params.data
+
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
+
+  return (
+    <div>
+      <p>Product: {productId}</p>
+      <p>Size: {size}</p>
+      <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
+    </div>
+  )
+}
